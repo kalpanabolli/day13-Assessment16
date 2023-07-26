0a8b60bf7479ef5d65db0a2f540acd96f417c01e
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-07-26 20:12:47 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13Assignment16
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter account holder's name: ");
+            string accountHolderName = Console.ReadLine();
+
+            BankAccount account = new BankAccount(accountHolderName);
+
+            Console.Write("Enter amount to deposit: ");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+
+            try
+            {
+                account.Deposit(depositAmount);
+                Console.WriteLine("Account Holder: " + account.AccountHolderName);
+                Console.WriteLine("Account Number: " + account.AccountNumber);
+                Console.WriteLine("Balance after deposit: $" + account.GetBalance());
+
+                Console.Write("Enter amount to withdraw: ");
+                double withdrawalAmount = Convert.ToDouble(Console.ReadLine());
+
+                try
+                {
+                    account.Withdraw(withdrawalAmount);
+                    Console.WriteLine("Balance after withdrawal: $" + account.GetBalance());
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+
+            }
+        }
+    }
+}
